4f2908085222dcdef056bd7f40cbe0bed0d67602
Joakim Durehed <joakim.durehed@gmail.com>
2014-08-05 21:04:26 +0200

    Updated frontpage

--- laravel/app/views/pages/home.blade.php
+++ laravel/app/views/pages/home.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.default')
 
 @section('content')
-   <h1>Welcome</h1>
+<div class="jumbotron">
+    <h1>Larabook</h1>
+    <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+    <p>
+        <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Sign up &raquo;</a>
+    </p>
+</div>
 @stop
