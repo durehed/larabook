b0e57a0b12c0bfe11a285f5e8b1e2778d44614f3
Joakim Durehed <joakim.durehed@gmail.com>
2014-08-05 20:59:34 +0200

    gulp ready

--- laravel/Gulpfile.js
+++ laravel/Gulpfile.js
@@ -7,8 +7,17 @@ var autoprefixer = require('gulp-autoprefixer');
 
 
 // Compile sass and store it in public/css folder
-gulp.task('css',function() {
+gulp.task('css', function() {
    gulp.src('app/assets/sass/main.scss')
        .pipe(sass())
+       .pipe(autoprefixer('last 10 versions'))
        .pipe(gulp.dest('public/css'));
 });
+
+// Watch for changes
+gulp.task('watch', function() {
+    gulp.watch('app/assets/sass/**/*.scss', ['css']);
+});
+
+// Default task
+gulp.task('default', ['watch']);
